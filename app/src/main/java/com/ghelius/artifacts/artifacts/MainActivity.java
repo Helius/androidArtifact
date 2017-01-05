aac9b09ee32fe55ca0f94d30338d7ab7b45fc7fe
Eugene Samoylov <e.samoylov@2Gis.ru>
2017-01-05 18:01:07 +0700

    add navigation drawer, WIP

--- app/src/main/java/com/ghelius/artifacts/artifacts/MainActivity.java
+++ app/src/main/java/com/ghelius/artifacts/artifacts/MainActivity.java
@@ -2,26 +2,65 @@ package com.ghelius.artifacts.artifacts;
 
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
     private static final String TAG="mainActivity";
     ChooseAuthorGameFragment chooseAuthorGameFragment;
-    ActionBar actionBar;
+//    ActionBar actionBar;
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         MainMenuFragment mainMenuFragment = new MainMenuFragment();
 
@@ -29,17 +68,38 @@ public class MainActivity extends AppCompatActivity {
                 .add(R.id.main_fragment_holder, mainMenuFragment)
                 .commit();
 
+
+
         getSupportFragmentManager().addOnBackStackChangedListener(new FragmentManager.OnBackStackChangedListener() {
             @Override
             public void onBackStackChanged() {
+                ActionBar actionBar = getSupportActionBar();
                 if (getSupportFragmentManager().getBackStackEntryCount() != 0) {
-                    if (actionBar != null) {
-                    }
+//                    actionBar.setDisplayHomeAsUpEnabled(true);
+                } else {
+//                    actionBar.setDisplayHomeAsUpEnabled(false);
                 }
             }
         });
 
+    }
 
+    //TODO delete this shit
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
@@ -68,7 +128,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void launchChooseAuthor(View view) {
-        chooseAuthorGameFragment = new ChooseAuthorGameFragment();
+        if (chooseAuthorGameFragment == null) {
+            chooseAuthorGameFragment = new ChooseAuthorGameFragment();
+        }
 
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.main_fragment_holder, chooseAuthorGameFragment)
