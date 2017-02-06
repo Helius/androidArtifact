0e5ff3106ad3fda9d75d30823695a61a808fdeec
Eugene Samoylov <e.samoylov@2Gis.ru>
2017-02-06 22:05:15 +0700

    create instagram-like full image preview =)

--- app/src/main/java/com/ghelius/artifacts/artifacts/ChoosePaintGameFragment.java
+++ app/src/main/java/com/ghelius/artifacts/artifacts/ChoosePaintGameFragment.java
@@ -3,6 +3,8 @@ package com.ghelius.artifacts.artifacts;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
@@ -10,8 +12,12 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.Transformation;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
@@ -19,6 +25,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
@@ -26,12 +36,12 @@ import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Locale;
 import java.util.Random;
 
 import static com.ghelius.artifacts.artifacts.ChoosePaintGameFragment.ButtonState.False;
 import static com.ghelius.artifacts.artifacts.ChoosePaintGameFragment.ButtonState.Hide;
-import static com.ghelius.artifacts.artifacts.ChoosePaintGameFragment.ButtonState.Normal;
 import static com.ghelius.artifacts.artifacts.ChoosePaintGameFragment.ButtonState.True;
 
 
@@ -51,6 +61,7 @@ public class ChoosePaintGameFragment extends Fragment implements GameSetFinished
     private ArrayList<ChoosePaintGame> games = null;
     private int gameIndex;
     GameSetFinishedDialog dialog;
+    ImageView fullImage;
     private int GameCount = 10;
     private int trueAnswerCount;
     private String locale;
@@ -63,6 +74,8 @@ public class ChoosePaintGameFragment extends Fragment implements GameSetFinished
         Picture picture;
         ButtonState state;
         Long author_id;
+        String url;
+        Bitmap cachedBitmap;
 
         ChooseButton(Picture picture, Long author_id) {
             this.picture = picture;
@@ -97,33 +110,43 @@ public class ChoosePaintGameFragment extends Fragment implements GameSetFinished
         }
 
         @Override
-        public View getView(int i, View view, ViewGroup viewGroup) {
+        public View getView(int i, View view, final ViewGroup viewGroup) {
             if (view == null) {
                 view = mInflater.inflate(R.layout.choose_paint_item, viewGroup, false);
             }
-            ChooseButton button = mButtons.get(i);
+            Log.d(TAG, "getView " + i);
+            final ChooseButton button = mButtons.get(i);
             final ImageView pic = (ImageView) view.findViewById(R.id.picture);
-            mStorageRef.child(button.picture.path).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                @Override
-                public void onSuccess(Uri uri) {
-                    Activity a = getActivity();
-                    if (a == null)
-                        return;
+            pic.setImageBitmap(mButtons.get(i).cachedBitmap);
+//            if (button.url == null) {
+//                mStorageRef.child(button.picture.path).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+//                    @Override
+//                    public void onSuccess(Uri uri) {
+//                        button.url = uri.toString();
+//                        Activity a = getActivity();
+//                        if (a == null)
+//                            return;
 
-                    Glide.with(a.getApplicationContext())
-                            .load(uri.toString())
-                            .asBitmap()
-                            .centerCrop()
-                            .into(pic);
+//                        Log.d(TAG, "loading image ");
+//                        Glide.with(a.getApplicationContext())
+//                                .using(new FirebaseImageLoader())
+//                                .load(mStorageRef.child(button.picture.path))
+//                                .diskCacheStrategy(DiskCacheStrategy.ALL)
+//                                .placeholder(R.drawable.progress_circle)
+//                                .crossFade()
+////                            .centerCrop()
+//                                .into(pic);
 
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                                        @Override
-                                        public void onFailure(@NonNull Exception exception) {
+//                    }
+////                }).addOnFailureListener(new OnFailureListener() {
+////                                            @Override
+////                                            public void onFailure(@NonNull Exception exception) {
+////
+////                                            }
+////                                        }
+////                );
+//            }
 
-                                        }
-                                    }
-            );
 
             switch (button.state) {
                 case True:
@@ -142,6 +165,10 @@ public class ChoosePaintGameFragment extends Fragment implements GameSetFinished
         }
 
         void update(int i) {
+            Log.d(TAG, "update grid");
+            for (ChooseButton button : mButtons) {
+                button.url = null;
+            }
             notifyDataSetChanged();
         }
     }
@@ -180,7 +207,7 @@ public class ChoosePaintGameFragment extends Fragment implements GameSetFinished
         // Inflate the layout for this fragment
         locale = Locale.getDefault().getLanguage();
         View view = inflater.inflate(R.layout.fragment_choose_paint_game, container, false);
-        GridView mGridView = (GridView) view.findViewById(R.id.paint_grid);
+        final GridView mGridView = (GridView) view.findViewById(R.id.paint_grid);
         author_view = (TextView) view.findViewById(R.id.author);
         mButtons = new ArrayList<>();
         mButtonAdapter = new ButtonAdapter(getActivity().getApplicationContext(), mButtons);
@@ -191,6 +218,31 @@ public class ChoosePaintGameFragment extends Fragment implements GameSetFinished
                 buttonSelected(i);
             }
         });
+        mGridView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                showFullImageDialog(i);
+                return true;
+            }
+        });
+        mGridView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_UP:
+//                    case MotionEvent.ACTION_CANCEL:
+                        hideFullImageDialog();
+//                        mGridView.clearChoices();
+                        return false;
+//                    case MotionEvent.ACTION_DOWN:
+                    default:
+                        break;
+
+                }
+                return false;
+            }
+        });
+
         if (games == null) {
             games = createNewGame(GameCount);
         }
@@ -201,9 +253,82 @@ public class ChoosePaintGameFragment extends Fragment implements GameSetFinished
         dialog.init(trueAnswerCount, GameCount, 0, false);
         dialog.setEventListener(this);
 
+        fullImage = (ImageView) view.findViewById(R.id.full_image);
+
         return view;
     }
 
+    private void hideFullImageDialog() {
+        int width  = fullImage.getMeasuredWidth();
+        int height  = fullImage.getMeasuredHeight();
+        SizeChangeAnimation anim = new SizeChangeAnimation(fullImage);
+        anim.setHeights(width, 0);
+        anim.setWidths(height, 0);
+        anim.setDuration(100);
+        fullImage.startAnimation(anim);
+    }
+
+    private void showFullImageDialog(final int i) {
+        Activity a = getActivity();
+        if (a == null)
+            return;
+
+        fullImage.setVisibility(View.VISIBLE);
+        fullImage.setImageBitmap(mButtons.get(i).cachedBitmap);
+        View layout = getView().findViewById(R.id.choose_pict_root_layout);
+        int width  = layout.getMeasuredWidth();
+        int height  = layout.getMeasuredHeight();
+        SizeChangeAnimation anim = new SizeChangeAnimation(fullImage);
+        anim.setHeights(10, width);
+        anim.setWidths(10, height);
+        anim.setDuration(150);
+        fullImage.startAnimation(anim);
+    }
+public class SizeChangeAnimation extends Animation {
+
+    private int startHeight;
+    private int deltaHeight;
+
+    private int startWidth;
+    private int deltaWidth;
+
+    private View view;
+
+    public SizeChangeAnimation(View view) {
+        this.view = view;
+    }
+
+    public void setHeights(int start, int end) {
+        this.startHeight = start;
+        this.deltaHeight = end - this.startHeight;
+    }
+
+    public void setWidths(int start, int end) {
+        this.startWidth = start;
+        this.deltaWidth = end - this.startWidth;
+    }
+
+    @Override
+    protected void applyTransformation(float interpolatedTime, Transformation t) {
+        if (startHeight != 0) {
+            if (deltaHeight > 0) {
+                view.getLayoutParams().height = (int) (startHeight + deltaHeight * interpolatedTime);
+            } else {
+                view.getLayoutParams().height = (int) (startHeight - Math.abs(deltaHeight) * interpolatedTime);
+            }
+        }
+
+        if (startWidth != 0) {
+            if (deltaWidth > 0) {
+                view.getLayoutParams().width = (int) (startWidth + deltaWidth * interpolatedTime);
+            } else {
+                view.getLayoutParams().width = (int) (startWidth - Math.abs(deltaWidth) * interpolatedTime);
+            }
+        }
+
+        view.requestLayout();
+    }
+}
     @Override
     public void onStart() {
         super.onStart();
@@ -246,8 +371,6 @@ public class ChoosePaintGameFragment extends Fragment implements GameSetFinished
                     mButtons.get(i).state = False;
                 }
             }
-//            mButtons.get(ind).state = False;
-//
         }
         mButtonAdapter.update(ind);
 
@@ -311,10 +434,24 @@ public class ChoosePaintGameFragment extends Fragment implements GameSetFinished
             }
         } else { // we played all game and now just show last one
         }
-        mButtonAdapter.update(0);
+        loadAllImages();
     }
 
-
+    void loadAllImages() {
+        for (final ChooseButton b : mButtons) {
+            Glide.with(getActivity())
+                    .using(new FirebaseImageLoader())
+                    .load(mStorageRef.child(b.picture.path))
+                    .asBitmap()
+                    .into(new SimpleTarget<Bitmap>() {
+                        @Override
+                        public void onResourceReady(Bitmap resource, GlideAnimation glideAnimation) {
+                            b.cachedBitmap = resource;
+                            mButtonAdapter.update(0);
+                        }
+                    });
+        }
+    }
 
     ArrayList<ChoosePaintGame> createNewGame(int count)
     {
@@ -363,49 +500,12 @@ public class ChoosePaintGameFragment extends Fragment implements GameSetFinished
         Author author;
         ArrayList<Picture> picture_variant;
         private int id;
-
-
-//        SimpleTarget<Bitmap> target = new SimpleTarget<Bitmap>(300,300) {
-//            @Override
-//            public void onResourceReady(Bitmap bitmap, GlideAnimation glideAnimation) {
-//                Log.d(TAG, "helius: image " + id + "loaded from ??");
-//                if (id == gameIndex) {
-//                    mImageView.setImageBitmap(bitmap);
-//                    showButtonBlock(true);
-//                    View v = getView();
-//                    if (v != null) {
-//                        getView().findViewById(R.id.progress_view).setVisibility(View.GONE);
-//                    }
-//                }
-//            }
-//        };
+        public HashMap<String, Bitmap> cachedPicture;
 
         ChoosePaintGame(int id) {
             this.id = id;
             this.picture_variant = new ArrayList<>();
+            this.cachedPicture = new HashMap<>();
         }
-
-//        void loadPicture(int index, final ImageView view) {
-//            mStorageRef.child(picture_variant.get(index).path).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-//                @Override
-//                public void onSuccess(Uri uri) {
-//                    Activity a = getActivity();
-//                    if (a == null)
-//                        return;
-//
-//                    Glide.with(a.getApplicationContext())
-//                            .load(uri.toString())
-//                            .asBitmap()
-//                            .into(view);
-//
-//                }
-//            }).addOnFailureListener(new OnFailureListener() {
-//                @Override
-//                public void onFailure(@NonNull Exception exception) {
-//                    // Handle any errors
-//                }
-//            });
-//        }
     }
-
 }
